5a3d168f19d62eee302b1d9bab02a4de61261414
Darshan <darshanm.itpath@gmail.com>
2024-01-24 16:53:44 +0530

    new folder structure changes

---
+++ Cinemax/Cinemax/Extensions/Storyboards+Extensions.swift
@@ -0,0 +1,15 @@
+//
+//  Storyboards+Extensions.swift
+//  Cinemax
+//
+//  Created by IPS-161 on 24/01/24.
+//
+
+import Foundation
+import UIKit
+
+extension UIStoryboard {
+    static let Onboarding = UIStoryboard(name: "Onboarding", bundle: nil)
+    static let Authentication = UIStoryboard(name: "Authentication", bundle: nil)
+    static let MainTab = UIStoryboard(name: "MainTab", bundle: nil)
+}
