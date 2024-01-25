687cf4f224c48e7a9f9229ef82e136e72b14c702
Darshan <darshanm.itpath@gmail.com>
2024-01-25 14:19:34 +0530

    ne whccnages

--- Cinemax/Cinemax/Screens/Onboarding/OnboardingVC.swift
+++ Cinemax/Cinemax/Screens/Onboarding/OnboardingVC.swift
@@ -13,17 +13,35 @@ protocol OnboardingVCProtocol: class {
 
 class OnboardingVC: UIViewController {
     
+    @IBOutlet weak var backgroundImg: UIImageView!
+    @IBOutlet weak var frontImg: UIImageView!
+    @IBOutlet weak var frontImgView: UIView!
+    @IBOutlet weak var headlineLbl: UILabel!
+    @IBOutlet weak var subHeadlineLbl: UILabel!
+    @IBOutlet weak var pageControl: UIPageControl!
+    
     var presenter: OnboardingVCPresenterProtocol?
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         presenter?.viewDidload()
     }
     
+    @IBAction func nxtBtnPressed(_ sender: UIButton) {
+        
+    }
+    
+    
 }
 
 extension OnboardingVC: OnboardingVCProtocol {
     func updateUI(){
-        print("updateUI")
+        guard let datasource = presenter?.datasource[0] else {
+            return
+        }
+        backgroundImg.image = datasource.img
+        headlineLbl.text = datasource.headline
+        subHeadlineLbl.text = datasource.subHeadline
     }
 }
