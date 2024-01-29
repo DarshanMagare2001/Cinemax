d25664cd61e8ebafa54a7e71e747d0aefe33d8a4
Darshan <darshanm.itpath@gmail.com>
2024-01-29 14:26:08 +0530

    new changes

--- Cinemax/Cinemax/Screens/Authentication/SignUpCredentialVC/SignUpCredentialVC.swift
+++ Cinemax/Cinemax/Screens/Authentication/SignUpCredentialVC/SignUpCredentialVC.swift
@@ -13,7 +13,16 @@ protocol SignUpCredentialVCProtocol: class {
 
 class SignUpCredentialVC: UIViewController {
     
+    @IBOutlet weak var fullNameTxtFld: UITextField!
+    @IBOutlet weak var emailaddressTxtFld: UITextField!
+    @IBOutlet weak var passwordTxtFld: UITextField!
+    @IBOutlet weak var passwordShowHideBtn: UIButton!
+    @IBOutlet weak var privacyAndPolicyBtn: UIButton!
+    @IBOutlet weak var termsANdConditionLbl: UILabel!
+    
     var presenter: SignUpCredentialVCPresenterProtocol?
+    var isPassworShow = false
+    var isTermsAndConditionCheck = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,7 +33,20 @@ class SignUpCredentialVC: UIViewController {
         navigationController?.popViewController(animated: true)
     }
     
-  
+    @IBAction func passwordShowHideBtnPressed(_ sender: UIButton) {
+        isPassworShow =  isPassworShow ? false : true
+        passwordShowHideBtn.setImage(UIImage(named: isPassworShow ? "EyeBtnOpen" : "EyeBtnClose"), for: .normal)
+    }
+    
+    @IBAction func privacyAndPolicyBtnPressed(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func signUpBtnPressed(_ sender: UIButton) {
+        
+    }
+    
+    
 }
 
 extension SignUpCredentialVC: SignUpCredentialVCProtocol {
