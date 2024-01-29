255ab095d25ff27ae393ddbeb4a35581ed103648
Darshan <darshanm.itpath@gmail.com>
2024-01-29 18:25:29 +0530

    ne wchnages

--- Cinemax/Cinemax/Screens/Authentication/ResetPassword/ResetPasswordVC.swift
+++ Cinemax/Cinemax/Screens/Authentication/ResetPassword/ResetPasswordVC.swift
@@ -8,29 +8,33 @@
 import UIKit
 
 class ResetPasswordVC: UIViewController {
-    
+
     @IBOutlet weak var resetPasswordView: UIView!
     @IBOutlet weak var verificationView: UIView!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         resetPasswordView.isHidden = false
         verificationView.isHidden = true
     }
-    
-    
+
     @IBAction func nxtBtnPressed(_ sender: UIButton) {
-        verificationView.isHidden.toggle()
-        resetPasswordView.isHidden.toggle()
+        UIView.transition(with: view, duration: 0.5, options: .transitionFlipFromRight, animations: {
+            // Toggle visibility after animation completion
+            self.verificationView.isHidden.toggle()
+            self.resetPasswordView.isHidden.toggle()
+        }, completion: nil)
     }
-    
+
     @IBAction func resendBtnPressed(_ sender: UIButton) {
-        verificationView.isHidden.toggle()
-        resetPasswordView.isHidden.toggle()
+        UIView.transition(with: view, duration: 0.5, options: .transitionFlipFromRight, animations: {
+            // Toggle visibility after animation completion
+            self.verificationView.isHidden.toggle()
+            self.resetPasswordView.isHidden.toggle()
+        }, completion: nil)
     }
-    
-    func backBtnPressed(){
+
+    func backBtnPressed() {
         navigationController?.popViewController(animated: true)
     }
-    
 }
