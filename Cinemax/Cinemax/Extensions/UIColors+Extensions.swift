8c616d5162e077b4a759f0c375724f5cf7c231d3
Darshan <darshanm.itpath@gmail.com>
2024-01-31 19:06:17 +0530

    final push of wed 31 jan 2023

--- Cinemax/Cinemax/Extensions/UIColors+Extensions.swift
+++ Cinemax/Cinemax/Extensions/UIColors+Extensions.swift
@@ -12,4 +12,5 @@ extension UIColor {
     static let appBlue = UIColor(named: "AppBlue")
     static let appOrange = UIColor(named: "AppOrange")
     static let appBlue2 = UIColor(named: "AppBlue2")
+    static let appDark1 = UIColor(named: "AppDark1")
 }
--- Cinemax/Cinemax/Screens/MainTab/MainTabVC.swift
+++ Cinemax/Cinemax/Screens/MainTab/MainTabVC.swift
@@ -14,16 +14,57 @@ class MainTabVC: UIViewController {
     @IBOutlet weak var searchBtnLbl: UILabel!
     @IBOutlet weak var downloadBtnLbl: UILabel!
     @IBOutlet weak var profileBtnLbl: UILabel!
+    @IBOutlet weak var homeBtn: UIButton!
+    @IBOutlet weak var homeBtnView: RoundedCornerView!
+    @IBOutlet weak var searchBtnView: RoundedCornerView!
+    @IBOutlet weak var searchBtn: UIButton!
+    @IBOutlet weak var downloadBtnView: RoundedCornerView!
+    @IBOutlet weak var downloadBtn: UIButton!
+    @IBOutlet weak var personBtnView: RoundedCornerView!
+    @IBOutlet weak var personBtn: UIButton!
     
+    var isHomeTabShow = true {
+        didSet{
+            homeBtnView.borderColor = isHomeTabShow ? UIColor.appBlue! : UIColor.black
+            homeBtnView.backgroundColor = isHomeTabShow ? UIColor.appDark1! : UIColor.black
+            homeBtn.tintColor = isHomeTabShow ? UIColor.appBlue! : UIColor.black
+            homeBtnLbl.textColor = isHomeTabShow ? UIColor.appBlue! : UIColor.black
+        }
+    }
+   
     override func viewDidLoad() {
         super.viewDidLoad()
-        homeBtnLbl.isHidden = true
-//        searchBtnLbl.isHidden = true
-        downloadBtnLbl.isHidden = true
-        profileBtnLbl.isHidden = true
+        setUpTabs()
     }
     
     
+    @IBAction func homeBtnPressed(_ sender: UIButton) {
+        
+    }
+    
+    
+    @IBAction func searchBtnPressed(_ sender: UIButton) {
+        
+    }
+    
+    
+    @IBAction func downloadBtnPressed(_ sender: UIButton) {
+        
+    }
+    
+    
+    @IBAction func personBtnPressed(_ sender: UIButton) {
+        
+    }
     
 }
 
+extension MainTabVC {
+    private func setUpTabs(){
+        isHomeTabShow = true
+        homeBtnLbl.isHidden = false
+        searchBtnLbl.isHidden = true
+        downloadBtnLbl.isHidden = true
+        profileBtnLbl.isHidden = true
+    }
+}
