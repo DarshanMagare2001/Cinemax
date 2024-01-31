84443dd871e34751fc64485bf38afadba1171aee
Darshan <darshanm.itpath@gmail.com>
2024-01-31 12:46:12 +0530

    new chanegs

--- Cinemax/Cinemax/Screens/Authentication/Login/LoginVC.swift
+++ Cinemax/Cinemax/Screens/Authentication/Login/LoginVC.swift
@@ -14,32 +14,42 @@ protocol LoginVCProtocol: class {
 class LoginVC: UIViewController {
     
     @IBOutlet weak var headingLbl: UILabel!
+    @IBOutlet weak var emailAdressTxtFld: UITextField!
+    @IBOutlet weak var emailWarningLbl: RoundedLabelWithBorder!
+    @IBOutlet weak var passwordTxtFld: UITextField!
+    @IBOutlet weak var passwordShowHideBtn: UIButton!
+    @IBOutlet weak var passwordWarningLbl: RoundedLabelWithBorder!
+    @IBOutlet weak var logInBtn: RoundedButton!
     
     var presenter: LoginVCPresenterProtocol?
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         presenter?.viewDidload()
     }
-  
+    
     
     @IBAction func forgetPasswordBtnPrressed(_ sender: UIButton) {
         presenter?.goToResetPasswordVC()
     }
     
     
-    func backBtnPressed(){
-        navigationController?.popViewController(animated: true)
+    @IBAction func logInBtnPressed(_ sender: UIButton) {
+        
     }
-
+    
 }
 
 extension LoginVC: LoginVCProtocol {
     
     func updateUI(){
         if let name = UserdefaultRepositoryManager.fetchUserInfoFromUserdefault(type: .currentUsersName){
-            headingLbl.text = "Hi, \(name)"
+            headingLbl.text = "Hi,\(name)"
         }
+    }
+    
+    func backBtnPressed(){
+        navigationController?.popViewController(animated: true)
     }
     
 }
