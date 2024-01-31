6f6557df1182c5855927a3420b45ec326abaf6c7
Darshan <darshanm.itpath@gmail.com>
2024-01-31 18:43:45 +0530

    new changes

--- Cinemax/Cinemax/Screens/MainTab/MainTabVC.swift
+++ Cinemax/Cinemax/Screens/MainTab/MainTabVC.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class MainTabVC: UIViewController {
-
+    
+    @IBOutlet weak var mainView: UIView!
+    @IBOutlet weak var homeBtnLbl: UILabel!
+    @IBOutlet weak var searchBtnLbl: UILabel!
+    @IBOutlet weak var downloadBtnLbl: UILabel!
+    @IBOutlet weak var profileBtnLbl: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
- 
+        homeBtnLbl.isHidden = true
+//        searchBtnLbl.isHidden = true
+        downloadBtnLbl.isHidden = true
+        profileBtnLbl.isHidden = true
     }
-  
+    
     
     
 }
