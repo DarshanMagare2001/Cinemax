7a851237bfd536df3aeba2ec047d2f757ec30193
Darshan <darshanm.itpath@gmail.com>
2024-02-02 18:50:49 +0530

    new changes

--- Cinemax/Cinemax/Screens/MainTab/ProfileComponents/EditProfile/EditProfileVC.swift
+++ Cinemax/Cinemax/Screens/MainTab/ProfileComponents/EditProfile/EditProfileVC.swift
@@ -6,37 +6,41 @@
 //
 
 import UIKit
+import YPImagePicker
 
 protocol EditProfileVCProtocol : class {
     func setupUI(name:String,email:String)
 }
 
 class EditProfileVC: UIViewController {
-
+    
     @IBOutlet weak var fullNameTxtFld: UITextField!
     @IBOutlet weak var fullnameWarningLbl: RoundedLabelWithBorder!
     @IBOutlet weak var currentUserName: UILabel!
     @IBOutlet weak var currentUserEmailLbl: UILabel!
+    @IBOutlet weak var currentUserProfileImg: CircleImageView!
     
     var presenter: EditProfileVCPresenterProtocol?
+    var config = YPImagePickerConfiguration()
+    var imgPicker = YPImagePicker()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         presenter?.viewDidload()
     }
-
+    
     @IBAction func saveBtnPressed(_ sender: UIButton) {
         
     }
     
     @IBAction func profileImgEditBtn(_ sender: UIButton) {
-        
+        presentImagePicker()
     }
     
 }
 
 extension EditProfileVC : EditProfileVCProtocol {
-
+    
     func setupUI(name:String,email:String){
         currentUserName.text = name
         currentUserEmailLbl.text = email
@@ -44,6 +48,28 @@ extension EditProfileVC : EditProfileVCProtocol {
     
     func backBtnPressed(){
         navigationController?.popViewController(animated: true)
+    }
+    
+    func presentImagePicker() {
+        if let topViewController = UIApplication.topViewController() {
+            if topViewController.presentedViewController is YPImagePicker {
+                return
+            }
+        }
+        imgPicker.didFinishPicking { [weak self] items, cancelled in
+            for item in items {
+                switch item {
+                case .photo(let photo):
+                    let image = photo.image
+                    self?.currentUserProfileImg.image = image
+                    //                    self?.saveProfileImage(image:image)
+                case .video(let video):
+                    break
+                }
+            }
+            self?.dismiss(animated: true, completion: nil)
+        }
+        present(imgPicker, animated: true, completion: nil)
     }
     
 }
